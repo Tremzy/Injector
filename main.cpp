e3aeb708283d90cc72c2a82bd2e058998bbad56c
Tremzye <lichtblau.norbi6@gmail.com>
2025-05-15 21:03:47 +0200

    Finished basic gui and functions

--- main.cpp
+++ main.cpp
@@ -7,9 +7,19 @@
 #include <cwchar> 
 #include <tuple>
 #include <thread>
+#include <commdlg.h>
+
+#define SELECT_DLL_BUTTON 1
+#define INJECT_BUTTON 2
 
 HWND p_hwnd;
 std::map<int, std::tuple<const char*, int>> processes;
+static wchar_t selectedFilePath[MAX_PATH] = { 0 };
+HWND pathEdit;
+WNDPROC originalEditProc = nullptr;
+HHOOK g_hHook = NULL;
+bool consoleSwitch = false;
+HWND hComboBox = nullptr;
 
 void CreateConsole() {
     AllocConsole();
@@ -23,6 +33,18 @@ void CreateConsole() {
     std::cin.clear();
 }
 
+LRESULT CALLBACK KeyboardHook(int nCode, WPARAM wParam, LPARAM lParam) {
+    if (wParam == WM_KEYDOWN) {
+        KBDLLHOOKSTRUCT* keyPtr = reinterpret_cast<KBDLLHOOKSTRUCT*>(lParam);
+        DWORD vkCode = keyPtr->vkCode;
+        if (vkCode == VK_INSERT) {
+            consoleSwitch ? ShowWindow(GetConsoleWindow(), SW_HIDE) : ShowWindow(GetConsoleWindow(), SW_SHOW);
+            consoleSwitch = !consoleSwitch;
+        }
+    }
+    return CallNextHookEx(NULL, nCode, wParam, lParam);
+}
+
 DWORD FetchProcesses() {
     HANDLE snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
     if (snapshot == INVALID_HANDLE_VALUE) return 0;
@@ -32,7 +54,6 @@ DWORD FetchProcesses() {
 
     if (Process32First(snapshot, &processEntry)) {
         do {
-            //if (_wcsicmp(processEntry.szExeFile, processName) == 0) {
             //std::wcout << processEntry.szExeFile << L" - " << processEntry.th32ProcessID << std::endl;
             int procId = processEntry.th32ProcessID;
             wchar_t* w_fname = processEntry.szExeFile;
@@ -83,9 +104,26 @@ bool InjectDLL(DWORD processID, const std::string& dllPath) {
     return true;
 }
 
+LRESULT CALLBACK EditBoxProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+    switch (msg) {
+    case WM_LBUTTONDOWN:
+    case WM_LBUTTONDBLCLK:
+    case WM_RBUTTONDOWN:
+    case WM_RBUTTONDBLCLK:
+    case WM_MBUTTONDOWN:
+    case WM_SETFOCUS:
+    case WM_MOUSEACTIVATE:
+    case WM_KEYDOWN:
+    case WM_CHAR:
+    case WM_PASTE:
+        return 0;
+    }
+    return CallWindowProc(originalEditProc, hwnd, msg, wParam, lParam);
+}
+
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
-    HWND hComboBox;
-    HFONT hfont = CreateFont(16, 0, 0, 0, FW_NORMAL, FALSE, FALSE, FALSE, DEFAULT_CHARSET, OUT_DEFAULT_PRECIS, CLIP_DEFAULT_PRECIS, DEFAULT_QUALITY, DEFAULT_PITCH, L"Arial");
+    HFONT hfont = CreateFont(18, 0, 0, 0, FW_NORMAL, FALSE, FALSE, FALSE, DEFAULT_CHARSET, OUT_DEFAULT_PRECIS, CLIP_DEFAULT_PRECIS, DEFAULT_QUALITY, DEFAULT_PITCH, L"Calibri");
+    HFONT b_hfont = CreateFont(22, 0, 0, 0, FW_NORMAL, FALSE, FALSE, FALSE, DEFAULT_CHARSET, OUT_DEFAULT_PRECIS, CLIP_DEFAULT_PRECIS, DEFAULT_QUALITY, DEFAULT_PITCH, L"Calibri");
     switch (uMsg)
     {
     case WM_CREATE:{
@@ -93,32 +131,28 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) 
 
         RECT rect;
         GetWindowRect(hwnd, &rect);
+        HWND hButton = CreateWindow(L"BUTTON", L"Select DLL", WS_VISIBLE | WS_CHILD, (rect.right - rect.left) / 2 - 100, 100, 100, 30, hwnd, (HMENU)SELECT_DLL_BUTTON, GetModuleHandle(NULL), NULL);
+        HWND injectButton = CreateWindow(L"BUTTON", L"Inject", WS_VISIBLE | WS_CHILD, (rect.right - rect.left) / 2 - 100, 300, 200, 30, hwnd, (HMENU)INJECT_BUTTON, GetModuleHandle(NULL), NULL);
+        pathEdit = CreateWindow(L"EDIT", L"", WS_VISIBLE | WS_CHILD | WS_BORDER, (rect.right - rect.left) / 2 - 100, 160, 200, 25, hwnd, NULL, GetModuleHandle(NULL), NULL);
+        hComboBox = CreateWindowEx(0, L"COMBOBOX", NULL, CBS_DROPDOWNLIST | WS_CHILD | WS_VISIBLE | WS_VSCROLL, (rect.right - rect.left) / 2 - 100, 40, 200, 300, hwnd, NULL, GetModuleHandle(NULL), NULL);
 
-        hComboBox = CreateWindowEx(
-            0, L"COMBOBOX", NULL,
-            CBS_DROPDOWN | WS_CHILD | WS_VISIBLE | WS_VSCROLL,
-            (rect.right - rect.left) / 2 - 100, 40, 200, 300,
-            hwnd, NULL, GetModuleHandle(NULL), NULL
-        );
-
+        originalEditProc = (WNDPROC)SetWindowLongPtr(pathEdit, GWLP_WNDPROC, (LONG_PTR)EditBoxProc);
         SendMessage(hComboBox, WM_SETFONT, (WPARAM)hfont, TRUE);
+        SendMessage(hButton, WM_SETFONT, (WPARAM)hfont, TRUE);
+        SendMessage(pathEdit, WM_SETFONT, (WPARAM)hfont, TRUE);
+        SendMessage(injectButton, WM_SETFONT, (WPARAM)b_hfont, TRUE);
         
         for (auto& process : processes) {
-            //extracting info from current entry
             std::tuple<const char*, int> processData = process.second;
             const char* procName = std::get<0>(processData);
             int procId = std::get<1>(processData);
 
-            //building the text
             char option[260];
             std::snprintf(option, sizeof(option), "%s (%d)", procName, procId);
-
-            //converting to useable format
             wchar_t woption[300];
             size_t converted = 0;
             mbstowcs_s(&converted, woption, option, _TRUNCATE);
 
-            //appending to options
             SendMessage(hComboBox, CB_ADDSTRING, 0, (LPARAM)woption);
         }
         return 0;
@@ -140,9 +174,75 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) 
 
         DrawTextA(hdc, "Process list:", -1, &procListLabelRect, DT_SINGLELINE | DT_LEFT | DT_VCENTER);
 
+        RECT dllPathLabel = {
+            (rect.right - rect.left) / 2 - 90,
+            260,
+            50 + 200,
+            20 + 20
+        };
+
+        DrawTextA(hdc, "Current DLL basename:", -1, &dllPathLabel, DT_SINGLELINE | DT_LEFT | DT_VCENTER);
+
         EndPaint(hwnd, &ps);
         return 0;
     }
+    case WM_COMMAND:
+        if (LOWORD(wParam) == 1) {
+            OPENFILENAME ofn;
+            ZeroMemory(&ofn, sizeof(ofn));
+            ofn.lStructSize = sizeof(ofn);
+            ofn.hwndOwner = hwnd;
+            ofn.lpstrFile = selectedFilePath;
+            ofn.lpstrFile[0] = '\0';
+            ofn.nMaxFile = sizeof(selectedFilePath) / sizeof(wchar_t);
+            ofn.lpstrFilter = L"DLL Files (.dll)\0*.dll\0All Files\0*.*\0";
+            ofn.nFilterIndex = 1;
+            ofn.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST | OFN_NOCHANGEDIR;
+
+            if (GetOpenFileName(&ofn) == TRUE) {
+                std::wcout << selectedFilePath << std::endl;
+
+                wchar_t basename[260] = {0};
+                size_t pathLen = wcslen(selectedFilePath);
+
+                for (size_t i = pathLen; i > 0; i--) {
+                    if (selectedFilePath[i] == L'\\' || selectedFilePath[i] == L'/') {
+                        if (i != wcslen(selectedFilePath)) {
+                            wcsncpy_s(basename, &selectedFilePath[i + 1], _TRUNCATE);
+                            break;
+                        }
+                    }
+                }
+                SendMessage(pathEdit, EM_SETSEL, 0, -1);
+                SendMessage(pathEdit, EM_REPLACESEL, FALSE, (LPARAM)basename);
+            }
+        }
+        else if (LOWORD(wParam) == 2) {
+            int index = SendMessage(hComboBox, CB_GETCURSEL, 0, 0);
+            if (index != CB_ERR) {
+                wchar_t processData[260];
+                SendMessage(hComboBox, CB_GETLBTEXT, index, (LPARAM)processData);
+                std::wcout << processData << std::endl;
+
+                const wchar_t* openParen = wcsrchr(processData, L'(');
+                if (openParen) {
+                    int procID = _wtoi(openParen + 1);
+                    std::cout << procID << std::endl;
+
+                    size_t len = wcslen(selectedFilePath) + 1;
+                    char* ch_path = new char[len];
+                    size_t convChars = 0;
+                    wcstombs_s(&convChars, ch_path, len, selectedFilePath, _TRUNCATE);
+                    if (!InjectDLL((DWORD)procID, ch_path)) {
+                        MessageBox(hwnd, L"Failed to inject", L"Error", MB_ICONERROR);
+                    }
+                    else {
+                        MessageBox(hwnd, L"Successfully injected!", L"Success", MB_OK);
+                    }
+                }
+            }
+        }
+        return 0;
     case WM_DESTROY: {
         PostQuitMessage(0);
         return 0;
@@ -162,8 +262,11 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int nCmdShow) {
     RegisterClass(&wc);
     CreateConsole();
     FetchProcesses();
+    ShowWindow(GetConsoleWindow(), SW_HIDE);
+    g_hHook = SetWindowsHookExW(WH_KEYBOARD_LL, KeyboardHook, NULL, 0);
+    if (!g_hHook) MessageBox(NULL, L"Failed to install keyboard hook!\nBinds wont work", L"Error", MB_ICONERROR);
     HWND hwnd = CreateWindowEx(0, L"InjectorWindowClass", L"Injector", WS_SYSMENU | WS_VISIBLE, GetSystemMetrics(SM_CXSCREEN) / 2, GetSystemMetrics(SM_CYSCREEN) / 2, 300, 400, nullptr, nullptr, hInstance, nullptr);
-    if (!hwnd) return 1;
+    if (!hwnd) { MessageBox(NULL, L"Failed to create window!\nSomething may be blocking it!", L"Error", MB_ICONERROR);  return 1; }
     ShowWindow(hwnd, nCmdShow);
     
     p_hwnd = hwnd;
